ea8edc8024360227b67703bb1241d12f3132517f
Abhinov <abhinov.in@gmail.com>
2023-06-09 13:36:24 +0530

    deleted old arraylist

--- JavaDSA/src/DataStructures/list/DSxArrayList.java
+++ JavaDSA/src/DataStructures/list/DSxArrayList.java
@@ -2,35 +2,52 @@ package DataStructures.list;
 
 import java.util.Arrays;
 
-public class DSxArrayList {
+public class DSxArrayList<T> {
 
-    public final int RESIZE_FACTOR = 2;
-    public final float LOAD_FACTOR = 0.75F;
+    private static final int RESIZE_FACTOR = 2;
+    public final float LOAD_FACTOR = 1;
 
-    int size;
-    int capacity;
-    Object[] array;
+    private int capacity;
+    private int size;
+    private T[] array;
 
-    public DSxArrayList(int initialCapacity) {
-        this.capacity = initialCapacity;
-        array = new Object[initialCapacity];
+    public DSxArrayList() {
+        this(10);
     }
 
-    public Object add(Object element) {
-        if(size >= capacity * LOAD_FACTOR) {
+    public DSxArrayList(int capacity) {
+        if (capacity == 0) {
+            capacity = 10;
+        }
+
+        array = (T[]) new Object[capacity];
+        this.capacity = capacity;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public void add(T element) {
+        // check if the array is almost full
+        if (size >= capacity * LOAD_FACTOR) {
             resizeArray();
         }
-        array[size] = element;
-        return array[size++];
+        array[size++] = element;
     }
 
+    /**
+     * Create new array of larger length and copy contents to new Array
+     */
     private void resizeArray() {
         int newCapacity = capacity * RESIZE_FACTOR;
         array = Arrays.copyOf(array, newCapacity);
+        capacity = newCapacity;
     }
 
     @Override
     public String toString() {
-        return String.valueOf(Arrays.asList(array));
+        T[] printArray = Arrays.copyOf(array, size);
+        return Arrays.toString(printArray);
     }
 }
--- JavaDSA/src/DataStructures/list/Main.java
+++ JavaDSA/src/DataStructures/list/Main.java
@@ -3,7 +3,7 @@ package DataStructures.list;
 public class Main {
     public static void main(String[] args) {
 
-        DSxArrayList2<String> arrayList = new DSxArrayList2<>(1);
+        DSxArrayList<String> arrayList = new DSxArrayList<>(1);
 
         arrayList.add("a");
         arrayList.add("b");
