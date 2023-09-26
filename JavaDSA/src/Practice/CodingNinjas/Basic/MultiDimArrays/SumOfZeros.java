680a4c7dac8027b6fc195602e23369823194c000
abhinov-gogoi <abhinav.gogoi@clairvoyantsoft.com>
2023-09-26 07:19:36 +0530

    sum of zeros done

--- JavaDSA/src/Practice/CodingNinjas/Basic/MultiDimArrays/SumOfZeros.java
+++ JavaDSA/src/Practice/CodingNinjas/Basic/MultiDimArrays/SumOfZeros.java
@@ -7,16 +7,22 @@ import java.util.ArrayList;
  */
 public class SumOfZeros {
 
+    /**
+     * 1 0
+     * 0 1
+     */
     public static final ArrayList<ArrayList<Integer>> MATRIX_1 = new ArrayList<>() {
         {
             add(new ArrayList<Integer>() {
                 {
-                    add(1); add(0);
+                    add(1);
+                    add(0);
                 }
             });
             add(new ArrayList<Integer>() {
                 {
-                    add(0); add(1);
+                    add(0);
+                    add(1);
                 }
             });
         }
@@ -28,8 +34,75 @@ public class SumOfZeros {
 
 
     public static Integer coverageOfMatrix(ArrayList<ArrayList<Integer>> mat) {
+        int answer = 0;
+        for (int i = 0; i < mat.size(); i++) {
+            for (int j = 0; j < mat.get(i).size(); j++) {
+                int number = mat.get(i).get(j);
+//                System.out.println(number);
+                if (number == 0) {
+                    answer = answer + getSumOfAdjacentNumbers(i, j, mat);
+                }
+            }
 
-        // Write your code here.
-        return 0;
+        }
+
+        return answer;
+    }
+
+    /**
+     * only get the sum of top, bottom, left, right.
+     * DON'T take the cross(sideways) numbers
+     */
+    private static int getSumOfAdjacentNumbers(int i, int j, ArrayList<ArrayList<Integer>> mat) {
+        int sum = 0;
+//        top-left
+//        try {
+//            sum = sum + mat.get(i - 1).get(j - 1);
+//        } catch (Exception ignored) {
+//        }
+
+        // top-middle
+        try {
+            sum = sum + mat.get(i - 1).get(j);
+        } catch (Exception ignored) {
+        }
+
+        // top-right
+//        try {
+//            sum = sum + mat.get(i - 1).get(j + 1);
+//        } catch (Exception ignored) {
+//        }
+
+        // centre-left
+        try {
+            sum = sum + mat.get(i).get(j - 1);
+        } catch (Exception ignored) {
+        }
+
+        // centre-right
+        try {
+            sum = sum + mat.get(i).get(j + 1);
+        } catch (Exception ignored) {
+        }
+
+        // bottom-left
+//        try {
+//            sum = sum + mat.get(i + 1).get(j - 1);
+//        } catch (Exception ignored) {
+//        }
+
+        // bottom-middle
+        try {
+            sum = sum + mat.get(i + 1).get(j);
+        } catch (Exception ignored) {
+        }
+
+        // bottom-right
+//        try {
+//            sum = sum + mat.get(i + 1).get(j + 1);
+//        } catch (Exception ignored) {
+//        }
+
+        return sum;
     }
 }
--- JavaDSA/src/Practice/CodingNinjas/Basic/String/NextSmallestPalindrome.java
+++ JavaDSA/src/Practice/CodingNinjas/Basic/String/NextSmallestPalindrome.java
@@ -4,19 +4,82 @@ package Practice.CodingNinjas.Basic.String;
  * https://www.codingninjas.com/studio/problems/given-a-string-find-the-next-smallest-palindrome_874577
  * https://leetcode.com/problems/find-the-closest-palindrome/
  * https://www.geeksforgeeks.org/given-a-number-find-next-smallest-palindrome-larger-than-this-number/
+ * https://www.interviewbit.com/problems/next-smallest-palindrome/
+ *
  */
 public class NextSmallestPalindrome {
 
-    public static final String STR_1 = "1221";
+
+    public static final String STR_1 = "2";
     public static final String STR_2 = "999";
-    public static final String STR_3 = "23545";
+    public static final String STR_3 = "12321";
+    public static final String STR_4 = "23545";
 
     public static void main(String[] args) {
         System.out.println(nextPalindrome(STR_3, 0));
     }
 
-    public static String nextPalindrome(String number, int length) {
-        // Write your code here.
-        return null;
+    public static String nextPalindrome(String string, int length) {
+
+        // check if given string is actually a number
+        int number = Integer.MIN_VALUE;
+        try {
+            number = Integer.parseInt(string);
+        } catch (NumberFormatException nfe) {
+            System.out.println("Not a Number");
+            System.exit(0);
+        }
+
+        // there can be 4 cases -
+        if (string.length() == 1) {
+            // if number is single digit
+            number = number + 1;
+        } else if (stringContainsOnlyNines(string)) {
+            number = number + 2;
+        } else if (stringIsPalindrome(string)) {
+            number = incrementMidPoint(string);
+        } else {
+
+        }
+
+        return String.valueOf(number);
+    }
+
+    /**
+     * This method increments mid-point(s) of a number
+     * 1221 -> 1331
+     * 12321 -> 12421
+     */
+    private static int incrementMidPoint(String string) {
+        if(stringIsEvenLength(string)) {
+
+        } else {
+
+        }
+        return 0;
+    }
+
+    private static boolean stringIsEvenLength(String string) {
+        return string.length() % 2 == 0;
+    }
+
+    private static boolean stringContainsOnlyNines(String string) {
+        for(int i=0; i<string.length(); i++) {
+            if(string.charAt(i) != '9') {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static boolean stringIsPalindrome(String string) {
+        for(int i=0; i<string.length()/2; i++) {
+            char leftChar = string.charAt(i);
+            char rightChar = string.charAt(string.length()-1-i);
+            if(leftChar != rightChar) {
+                return false;
+            }
+        }
+        return true;
     }
 }
